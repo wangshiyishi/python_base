61818692a97cd160bee884c2dd16ec7f9074181b
wangshiyishi <13696139713@163.com>
2016-10-29 13:42:52 +0800

    second

---
+++ test2_1.py
@@ -0,0 +1,28 @@
+months = [
+'January',
+'February',
+'March',
+'April',
+'May',
+'June',
+'Juiy',
+'Augyst',
+'Septement',
+'October',
+'November',
+'December'
+
+]
+endings = ['st', 'nd', 'rd'] + 17 * ['th'] \
+         + ['st', 'nd', 'rd'] + 7 * ['th'] \
+         + ['st']
+year   = raw_input('year: ')
+month  = raw_input('Month (1-12): ')
+day    = raw_input('Day (1-31): ')
+month_number = int(month)
+day_number = int(day)
+month_name = months[month_number-1]
+ordinal = day + endings[day_number-1]
+
+print month_name + ' ' + ordinal + '. ' + year
+
