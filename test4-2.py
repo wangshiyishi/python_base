102053c6bef34ced2c03e8ff4a2ce7cda5c5ebd3
wangshiyishi <13696139713@163.com>
2016-10-31 16:05:17 +0800

    45

---
+++ test4-2.py
@@ -0,0 +1,26 @@
+people = {
+	'Alice':{
+	     'phone': '23145',
+	     'addr': 'For driver 23'
+	} 
+}
+
+labels = {
+	'phone': 'phone number',
+	'addr' : 'address'
+
+
+}
+name = raw_input('Name: ')
+
+request = raw_input('Phone number (p) or address (a)? ')
+
+key = request
+if request == 'p': key = 'phone'
+if request == 'a': key = 'addr'
+
+person = people.get(name, {})
+label = labels.get(key, key)
+result = person.get(key, 'not available')
+
+print "%s's %s is %s." % (name, label, result)
